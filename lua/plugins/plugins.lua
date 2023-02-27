72cfe0a6935e4f2cf2db55d12407932de28210de
reylee <>
2023-02-27 20:04:03 +0800

    add navigator and ufo plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -6,110 +6,89 @@ return {
     config = true,
     keys = { { "<A-r>", ":IncRename ", desc = "IncRename" } },
   },
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   opts = {
-  --     debug = false, -- set to true to enable debug logging
-  --     log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
-  --     -- default is  ~/.cache/nvim/lsp_signature.log
-  --     verbose = false, -- show debug line number
-  --
-  --     bind = true, -- This is mandatory, otherwise border config won't get registered.
-  --     -- If you want to hook lspsaga or other signature handler, pls set to false
-  --     doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
-  --     -- set to 0 if you DO NOT want any API comments be shown
-  --     -- This setting only take effect in insert mode, it does not affect signature help in normal
-  --     -- mode, 10 by default
-  --
-  --     floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
-  --
-  --     floating_window_above_cur_line = true, -- try to place the floating above the current line when possible
-  --     -- NOTE: Will set to true when fully tested, set to false will use whichever side has more space
-  --     -- NOTE: this setting will be helpful if you do not want the PUM and floating win overlap
-  --
-  --     floating_window_off_x = 1, -- adjust float windows x position.
-  --     floating_window_off_y = -1, -- adjust float windows y position.
-  --
-  --     fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
-  --     hint_enable = true, -- virtual hint enable
-  --     hint_prefix = "🐼 ", -- Panda for parameter
-  --     hint_scheme = "String",
-  --     hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-  --     max_height = 5, -- max height of signature floating_window, if content is more than max_height, you can scroll down
-  --     -- to view the hiding contents
-  --     max_width = 100, -- max_width of signature floating_window, line will be wrapped if exceed max_width
-  --     handler_opts = {
-  --       border = "rounded", -- double, rounded, single, shadow, none
-  --     },
-  --
-  --     always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
-  --
-  --     auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
-  --     extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-  --     zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
-  --
-  --     padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
-  --
-  --     transparency = 20, -- disabled by default, allow floating win transparent value 1~100
-  --     shadow_blend = 36, -- if you using shadow as border use this set the opacity
-  --     shadow_guibg = "Black", -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
-  --     timer_interval = 20, -- default timer check interval set to lower value if you want to reduce latency
-  --     toggle_key = "<A-s>", -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
-  --   },
-  -- },
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "VeryLazy",
-  --   config = function()
-  --     require("lsp_signature").setup({
-  --       bind = true, -- mandatory
-  --       doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
-  --       -- set to 0 if you DO NOT want any API comments be shown
-  --       -- This setting only take effect in insert mode, it does not affect signature help in normal
-  --       -- mode, 10 by default
-  --
-  --       max_height = 12, -- max height of signature floating_window
-  --       max_width = 80, -- max_width of signature floating_window
-  --       noice = true, -- set to true if you using noice to render markdown
-  --       wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
-  --
-  --       floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
-  --
-  --       floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
-  --       -- will set to true when fully tested, set to false will use whichever side has more space
-  --       -- this setting will be helpful if you do not want the PUM and floating win overlap
-  --
-  --       floating_window_off_x = 1, -- adjust float windows x position.
-  --       floating_window_off_y = 5, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
-  --       -- can be either number or function, see examples
-  --
-  --       close_timeout = 4000, -- close floating window after ms when laster parameter is entered
-  --       fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
-  --       hint_enable = true, -- virtual hint enable
-  --       hint_prefix = "🐼 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
-  --       hint_scheme = "String",
-  --       hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-  --       handler_opts = {
-  --         border = "rounded", -- double, rounded, single, shadow, none, or a table of borders
-  --       },
-  --
-  --       always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
-  --
-  --       auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
-  --       extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-  --       zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
-  --
-  --       padding = " ", -- character to pad on left and right of signature can be ' ', or '|'  etc
-  --
-  --       transparency = nil, -- disabled by default, allow floating win transparent value 1~100
-  --       shadow_blend = 36, -- if you using shadow as border use this set the opacity
-  --       shadow_guibg = "Black", -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
-  --       timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-  --       toggle_key = "<M-l>", -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
-  --
-  --       select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
-  --       move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
-  --     })
-  --   end,
-  -- },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = "kevinhwang91/promise-async",
+    event = "VeryLazy",
+    config = function()
+      vim.o.foldcolumn = "0"
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = -1
+      vim.o.foldenable = true
+
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+      vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+      vim.keymap.set("n", "zK", function()
+        local winid = require("ufo").peekFoldedLinesUnderCursor()
+        if not winid then
+          -- nvimlsp
+          vim.lsp.buf.hover()
+        end
+      end)
+
+      local handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = ("  %d "):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, { suffix, "MoreMsg" })
+        return newVirtText
+      end
+
+      require("ufo").setup({
+        fold_virt_text_handler = handler,
+        open_fold_hl_timeout = 500,
+        close_fold_kinds = { "imports", "comment" },
+        preview = {
+          win_config = {
+            border = { "", "─", "", "", "", "─", "", "" },
+            winhighlight = "Normal:Folded",
+            winblend = 0,
+          },
+          mappings = {
+            scrollU = "<C-u>",
+            scrollD = "<C-d>",
+            close = "q",
+            switch = "<Tab>",
+            trace = "<CR>",
+          },
+        },
+        provider_selector = function(bufnr, filetype)
+          return { "treesitter", "indent" }
+        end,
+      })
+    end,
+  },
+  {
+    "numToStr/Navigator.nvim",
+    lazy = false,
+    keys = {
+      { "<C-h>", "<CMD>NavigatorLeft<CR>", desc = "Navigator Left" },
+      { "<C-l>", "<CMD>NavigatorRight<CR>", desc = "Navigator Right" },
+      { "<C-k>", "<CMD>NavigatorUp<CR>", desc = "Navigator Up" },
+      { "<C-j>", "<CMD>NavigatorDown<CR>", desc = "Navigator Down" },
+    },
+    config = true,
+  },
 }
